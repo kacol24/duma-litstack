dc352b275410b8cceb8d6b414fd068099256c6d6
Kevin Chandra <kacol.bot@gmail.com>
2021-08-14 12:56:36 +0700

    hero banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,29 @@
 
 @section('seo_title', 'Home')
 
+@php($heroSlider = [
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+    asset('images/hero-1-why-duma.png'),
+])
+
 @section('content')
-    <div class="hero-slider container--full-hd">
-        <div class="container"
-             style="background: url({{ asset('images/bg-hero-slider.png') }}) no-repeat bottom center/cover">
-            <div class="hero-slider__slide">
-                <img src="{{ asset('images/hero-slide.png') }}" alt="" class="img-fluid w-100">
-            </div>
+    <div class="hero-slider container--full-hd"
+         style="background: url({{ asset('images/bg-hero-slider.png') }}) no-repeat bottom center/cover">
+        <div class="container px-0" data-slick>
+            @foreach($heroSlider as $slide)
+                <a href="#" class="hero-slider__slide">
+                    <img src="{{ $slide }}" alt="" class="img-fluid w-100">
+                </a>
+            @endforeach
         </div>
     </div>
     <section class="intro my-5 py-5">
@@ -43,7 +59,8 @@
                             Pengolahan Bahan Ideal
                         </h3>
                         <div class="blurb__content">
-                            Serbuk kayu DUMA<sup>®</sup> diperoleh dari limbah kayu yang dihasilkan dari produsen kayu, sehingga
+                            Serbuk kayu DUMA<sup>®</sup> diperoleh dari limbah kayu yang dihasilkan dari produsen kayu,
+                            sehingga
                             kita hanya menggunakan limbah kayu yang seharusnya akan dibuang di tempat pembuangan sampah.
                         </div>
                     </div>
@@ -58,7 +75,8 @@
                             Campuran Orisinilitas
                         </h3>
                         <div class="blurb__content">
-                            DUMA<sup>®</sup> menggunakan campuran bahan plastik PVC orisinil dan daur ulang untuk menciptakan
+                            DUMA<sup>®</sup> menggunakan campuran bahan plastik PVC orisinil dan daur ulang untuk
+                            menciptakan
                             produk dengan kekuatan dan performa terbaik.
                         </div>
                     </div>
@@ -73,7 +91,8 @@
                             Kualitas Terbaik
                         </h3>
                         <div class="blurb__content">
-                            DUMA<sup>®</sup> menggunakan campuran bahan plastik PVC orisinil dan daur ulang untuk menciptakan
+                            DUMA<sup>®</sup> menggunakan campuran bahan plastik PVC orisinil dan daur ulang untuk
+                            menciptakan
                             produk dengan kekuatan dan performa terbaik.
                         </div>
                     </div>
@@ -81,9 +100,9 @@
             </div>
         </div>
     </section>
-    <section class="why-duma position-relative">
+    <section class="why-duma position-relative container--full-hd">
+        <img src="{{ asset('images/bg-why-duma.png') }}" alt="" class="w-100 img-fluid">
         <div class="container">
-            <img src="{{ asset('images/bg-why-duma.png') }}" alt="" class="w-100 img-fluid">
             <div class="row justify-content-center d-block d-md-none">
                 <div class="col-md-10">
                     <div class="row justify-content-between py-md-5 my-md-5">
@@ -94,7 +113,8 @@
                         </div>
                         <div class="col-md-4">
                             <p>
-                                Mengapa DUMA<sup>®</sup> adalah sebuah material bahan bangunan untuk masa depan dan mengapa
+                                Mengapa DUMA<sup>®</sup> adalah sebuah material bahan bangunan untuk masa depan dan
+                                mengapa
                                 DUMA<sup>®</sup>
                                 adalah
                                 pilihan yang tepat untuk Anda.
@@ -118,7 +138,8 @@
                                 </div>
                                 <div class="col-md-4">
                                     <p>
-                                        Mengapa DUMA<sup>®</sup> adalah sebuah material bahan bangunan untuk masa depan dan mengapa
+                                        Mengapa DUMA<sup>®</sup> adalah sebuah material bahan bangunan untuk masa depan
+                                        dan mengapa
                                         DUMA<sup>®</sup>
                                         adalah
                                         pilihan yang tepat untuk Anda.
@@ -142,7 +163,8 @@
                         Lebih Dekat Dengan DUMA<sup>®</sup>?
                     </h3>
                     <p>
-                        Mengapa DUMA<sup>®</sup> adalah sebuah material bahan bangunan untuk masa depan dan mengapa DUMA<sup>®</sup> adalah
+                        Mengapa DUMA<sup>®</sup> adalah sebuah material bahan bangunan untuk masa depan dan mengapa DUMA<sup>®</sup>
+                        adalah
                         pilihan yang tepat untuk Anda.
                     </p>
                 </div>
